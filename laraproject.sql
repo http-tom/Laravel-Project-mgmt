fa1a29b7d8f162804371a12427867975a6325bfc
Tom <tom@freedom-web.co.uk>
2021-09-21 11:40:27 +0100

    Changed public folder to public_html. Removed hardcoded database name from sql file. Changed env file to be example env. Removed .env file from repo

--- laraproject.sql
+++ laraproject.sql
@@ -12,9 +12,6 @@ MySQL - 5.7.24-0ubuntu0.18.04.1 : Database - laraproject
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`laraproject` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-
-USE `laraproject`;
 
 /*Table structure for table `migrations` */
 
