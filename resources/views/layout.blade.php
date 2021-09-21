892402b112170de79499b4a22e3df8735ec91984
Tom <tom@freedom-web.co.uk>
2021-09-21 12:44:32 +0100

    App name comes from env filegs

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,7 +32,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="/">PROJECT-APP</a>
+                    <a class="navbar-brand" href="/">{{ getenv('APP_NAME') }}</a>
                 </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
