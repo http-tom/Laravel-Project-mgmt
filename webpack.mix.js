64b60b8057928f0af4426f50f55e1fa452366cf6
Tom <tom@freedom-web.co.uk>
2021-09-21 13:24:29 +0100

    Final changes for public -> public_html

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require('laravel-mix');
 
+mix.setPublicPath('public_html/');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +13,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public_html/js')
-   .sass('resources/assets/sass/app.scss', 'public_html/css');
+mix.js('resources/assets/js/app.js', 'js')
+   .sass('resources/assets/sass/app.scss', 'css');
